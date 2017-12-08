4e2c669c43f3b38b628a50103092d30f6511297a
mohamed mahmoud jedeine <moud@mohameds-MacBook-Air.local>
2017-12-07 20:08:52 -0500

    heroku

--- models/index.js
+++ models/index.js
@@ -7,9 +7,10 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  //var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  var sequelize = new Sequelize(config.dialect,config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 fs
   .readdirSync(__dirname)
