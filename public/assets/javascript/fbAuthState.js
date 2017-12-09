02579aeab11515adfc533444c6682c165e838d5f
mohamed mahmoud jedeine <moud@mohameds-MacBook-Air.local>
2017-12-09 01:14:38 -0500

    heroku

--- public/assets/javascript/fbAuthState.js
+++ public/assets/javascript/fbAuthState.js
@@ -25,7 +25,7 @@ initApp = function () {
 
             // Create a reference to this user's specific status node.
             // This is where we will store data about being online/offline.
-            var userStatusDatabaseRef = firebase.database().ref(`/status/${uid}`);
+            var userStatusDatabaseRef = firebase.database().ref('/status/'+uid);
 
             // We'll create two constants which we will write to 
             // the Realtime database when this device is offline
